fa5cbff9fe5bfa014115a9e346793d974752ef97
glugg23 <m.leonhardt424@gmail.com>
2018-11-23 10:14:06 +0000

    Changed project to use Springboot and Maven

---
+++ src/main/java/com/enucs/enucoin/EnucoinApplication.java
@@ -0,0 +1,12 @@
+package com.enucs.enucoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnucoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnucoinApplication.class, args);
+    }
+}
--- src/main/java/com/enucs/enucoin/Transaction.java
+++ src/main/java/com/enucs/enucoin/Transaction.java
@@ -1,3 +1,5 @@
+package com.enucs.enucoin;
+
 import org.json.JSONObject;
 
 import java.time.Instant;
